5b065b26c8cfe458c81e3f5c6b5ff702005d3321
ryborg <ryborg@users.noreply.github.com>
2021-03-21 17:06:33 -0400

    Update top_solid_block.go

--- protocol/command/top_solid_block.go
+++ protocol/command/top_solid_block.go
@@ -2,7 +2,7 @@ package command
 
 import (
 	"fmt"
-	"github.com/sandertv/mcwss/mctype"
+	"github.com/ryborg/mcwss/mctype"
 )
 
 // TopSolidBlockRequest produces the command required to request the top solid block at a location.
